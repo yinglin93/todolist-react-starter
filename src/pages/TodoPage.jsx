d4f29e81a2e96ee3abedad36ec6d6d1c4296caf0
yinglin93 <yrlin0015@gmail.com>
2024-07-26 21:03:15 +0800

    add current member at TodoPage

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -9,7 +9,7 @@ const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
   const [todos, setTodos] = useState([]);
   const navigate = useNavigate()
-  const { isAuthenticated } = useAuth()
+  const { isAuthenticated, currentMember } = useAuth()
 
   const handleChange = (value) => setInputValue(value);
   const handleAddTodo = async () => {
@@ -157,7 +157,7 @@ const TodoPage = () => {
   return (
     <div>
       TodoPage
-      <Header />
+      <Header username={currentMember?.name}/>
       <TodoInput
         inputValue={inputValue}
         onChange={handleChange}
