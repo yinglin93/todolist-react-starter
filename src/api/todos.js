1bc24c57fcea624be1628ce27e9739b0070b8924
yinglin93 <yrlin0015@gmail.com>
2024-08-05 21:26:58 +0800

    update todo by api

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,12 +1,29 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3004';
+const baseUrl = 'https://todo-list.alphacamp.io/api';
 
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  },
+);
 
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseUrl}/todos`);
+    const res = await axiosInstance.get(`${baseUrl}/todos`);
 
-    return res.data;
+    return res.data.data;
   } catch (error) {
     console.error('[Get Todos failed]: ', error);
   }
@@ -15,7 +32,7 @@ export const getTodos = async () => {
 export const createTodo = async (payload) => {
   const { title, isDone } = payload;
   try {
-    const res = await axios.post(`${baseUrl}/todos`, {
+    const res = await axiosInstance.post(`${baseUrl}/todos`, {
       title,
       isDone,
     });
@@ -28,9 +45,9 @@ export const createTodo = async (payload) => {
 export const patchTodo = async (payload) => {
   const { id, title, isDone } = payload;
   try{
-    const res = await axios.patch(`${baseUrl}/todos/${id}`,{
-    title, 
-    isDone,
+    const res = await axiosInstance.patch(`${baseUrl}/todos/${id}`, {
+      title,
+      isDone,
     });
     return res.data;
   } catch(error) {
@@ -40,7 +57,7 @@ export const patchTodo = async (payload) => {
 
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseUrl}/todos/${id}`)
+    const res = await axiosInstance.delete(`${baseUrl}/todos/${id}`);
     return res.data;
   } catch(error) {
     console.error('[Delete Todo failed]:', error);
