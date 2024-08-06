b6e3f05de828edfa6ed2b44548289a9e9dd1bcb2
yinglin93 <yrlin0015@gmail.com>
2024-08-06 10:54:35 +0800

    feat: fix BrowserRouter property

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.scss';
 import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const basename = process.env.REACT_APP_PUBLIC_URL;
+const basename = process.env.PUBLIC_URL;
 
 function App() {
   return (
